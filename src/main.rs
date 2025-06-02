dea782f37dfe6982f198d63bab16542c4dc29f3d
SDK256 <deepak.shah@bitpack.me>
2025-06-02 19:55:17 +0530

    read_header

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@
 use std::net::TcpListener;
 use std::io::{BufRead, BufReader, Write};
 
+
 /*
-The two headers (content type & content length) are required for the client to be able to parse the response body. 
+- Header names are case-insensitive.
+- The two headers (content type & content length) are required for the client to be able to parse the response body. 
 Note that each header ends in a CRLF, and the entire header section also ends in a CRLF.
 */
 
@@ -22,8 +24,22 @@ fn main() {
                 let mut request_line = String::new();
                 reader.read_line(&mut request_line).unwrap();
 
+                let mut user_agent = String::new();
+                let mut header_line = String::new();
+                
                 let parts : Vec<&str> = request_line.trim_end().split_whitespace().collect();
                 let path = parts.get(1).unwrap_or(&"");
+                
+                loop {
+                    header_line.clear();
+                    let bytes_read = reader.read_line(&mut header_line).unwrap();
+                    if (header_line == "\r\n" || bytes_read == 0){
+                        break;
+                    }
+                    if let Some(user_agent_stripped) = header_line.strip_prefix("user-agent: "){
+                        user_agent = user_agent_stripped.trim_end().to_string();
+                    }
+                }
 
                 let response = 
                 if *path == "/" {
@@ -33,6 +49,9 @@ fn main() {
                     let content = &path[6..];
                     let content_len = content.len();
                     format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", content_len, content)
+                }
+                else if *path == "/user-agent" {
+                    format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", user_agent.len(), user_agent)
                 }
                 else {
                     "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
