a2f7a9d47ab9bdf5b7e7ffa85216c5a50a00af20
SDK256 <deepak.shah@bitpack.me>
2025-06-02 15:36:15 +0530

    added response

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                if let Err(e) = stream.write_all(response.as_bytes()) {
+                    println!("Failed to write to stream: {}", e);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
