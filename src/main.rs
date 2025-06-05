a0817f3b30a14cec84357e4c7ce9564d74f398c8
SDK256 <deepak.shah@bitpack.me>
2025-06-05 17:30:34 +0530

    read_req_body

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::thread;
 use std::fs;
 use std::env;
 use std::path::PathBuf;
+
 
 /*
 - Header names are case-insensitive.
@@ -15,6 +16,7 @@ Note that each header ends in a CRLF, and the entire header section also ends in
 
 fn main() {
 
+    // this is to parse the --directory flags to fetch the filename
     let args : Vec<String> = env::args().collect();
     let mut directory = String::new();
     for i in 0..args.len() {
@@ -23,16 +25,21 @@ fn main() {
         }
     }
 
+    // the program listens to the specified port
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
+            /*
+            mutable borrow of stream is more appropriate here because reading a stream changes its internal state
+            Also, bufReader is used here which allocates a buffer and lets you read the data in small pieces instead of large chuncks
+            reading the data line-by-line or byte-by-byte from TcpStream is slow and inefficient, each call may result in syscall which is expensive
+
+            Imagine you are pouring water from a bucket, now:
+            - without buffering: you are taking one drop at a time
+            - with buffering: you fill a bucket(buffer) and scoop from it
+            */
             Ok(mut stream) => {
                 println!("accepted new connection");
-
-                /*
-                 mutable borrow of stream is more appropriate here because reading a stream changes its internal state                
-                */
-
                 let directory = directory.clone();
                 thread::spawn(move || {
                     let mut reader = BufReader::new(&mut stream);
@@ -44,15 +51,20 @@ fn main() {
                     
                     let parts : Vec<&str> = request_line.trim_end().split_whitespace().collect();
                     let path = parts.get(1).unwrap_or(&"");
-                    
+                    let method = parts.get(0).unwrap_or(&"");
+                    let content_length = 0; 
+
                     loop {
                         header_line.clear();
                         let bytes_read = reader.read_line(&mut header_line).unwrap();
-                        if (header_line == "\r\n" || bytes_read == 0){
+                        if header_line == "\r\n" || bytes_read == 0 {
                             break;
                         }
                         if let Some(user_agent_stripped) = header_line.strip_prefix("User-Agent: "){
                             user_agent = user_agent_stripped.trim_end().to_string();
+                        }
+                        if let Some(content_len) = header_line.strip_prefix("Content-Length: "){
+                            content_length = content_len.trim().parse::<usize>().unwrap_or(0);
                         }
                     }
 
@@ -69,13 +81,24 @@ fn main() {
                         format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", user_agent.len(), user_agent)
                     }
                     else if path.starts_with("/files/") {
-                        let file_name = &path[7..];
-                        let mut file_path = PathBuf::from(&directory);
-                        file_path.push(file_name);
-                        match fs::read_to_string(file_path) {
-                            Ok(file_bytes) => 
-                                format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}", file_bytes.len(), file_bytes),
-                            Err(_) => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+                        if *method == "POST" {
+                            let mut body = vec![0; content_length];
+                            reader.read_exact(&mut body).unwrap();
+                            let file_name = &path[7..];
+                            let mut file_path = PathBuf::from(&directory);
+                            file_path.push(file_name);
+                            fs::write(file_path, body).unwrap();
+                            "HTTP/1.1 201 Created\r\n\r\n".to_string()
+                        }
+                        else {
+                            let file_name = &path[7..];
+                            let mut file_path = PathBuf::from(&directory);
+                            file_path.push(file_name);
+                            match fs::read_to_string(file_path) {
+                                Ok(file_bytes) => 
+                                    format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}", file_bytes.len(), file_bytes),
+                                Err(_) => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+                            }
                         }
                     }
                     else {
